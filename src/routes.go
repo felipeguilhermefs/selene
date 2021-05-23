4dc4ef78b87fd348a94c4c34dd24f8e57c0d5726
Felipe Flores <felipeguilhermefs@gmail.com>
2021-05-23 10:45:57 -0300

    Organize routes

--- src/routes.go
+++ src/routes.go
@@ -7,42 +7,36 @@ import (
 
 // RegisterRoutes register all server routes
 func (s *Server) RegisterRoutes() {
-	loginView := view.NewView("login")
 	signupView := view.NewView("signup")
-	booksView := view.NewView("books")
-	newBookView := view.NewView("new_book")
-	bookView := view.NewView("book")
-
 	signupPage := handlers.HandleSignupPage(signupView)
 	s.router.HandleFunc("/signup", signupPage).Methods("GET")
-
 	signup := handlers.HandleSignup(signupView, s.services.Auth)
 	s.router.HandleFunc("/signup", signup).Methods("POST")
 
+	loginView := view.NewView("login")
 	loginPage := handlers.HandleLoginPage(loginView)
 	s.router.HandleFunc("/login", loginPage).Methods("GET")
-
 	login := handlers.HandleLogin(loginView, s.services.Auth)
 	s.router.HandleFunc("/login", login).Methods("POST")
 
 	logout := handlers.HandleLogout(s.services.Auth)
 	s.router.HandleFunc("/logout", s.middlewares.Login(logout)).Methods("POST")
 
+	booksView := view.NewView("books")
 	booksPage := handlers.HandleBooksPage(booksView, s.services.Book)
 	s.router.HandleFunc("/books", s.middlewares.Login(booksPage)).Methods("GET")
 
+	newBookView := view.NewView("new_book")
 	newBookPage := handlers.HandleNewBookPage(newBookView)
 	s.router.HandleFunc("/books/new", s.middlewares.Login(newBookPage)).Methods("GET")
-
 	newBook := handlers.HandleNewBook(newBookView, s.services.Book)
 	s.router.HandleFunc("/books/new", s.middlewares.Login(newBook)).Methods("POST")
 
+	bookView := view.NewView("book")
 	editBookPage := handlers.HandleBookPage(bookView, s.services.Book)
 	s.router.HandleFunc("/books/{id:[0-9]+}", s.middlewares.Login(editBookPage)).Methods("GET")
-
 	editBook := handlers.HandleEditBook(bookView, s.services.Book)
 	s.router.HandleFunc("/books/{id:[0-9]+}/edit", s.middlewares.Login(editBook)).Methods("POST")
-
 	deleteBook := handlers.HandleDeleteBook(bookView, s.services.Book)
 	s.router.HandleFunc("/books/{id:[0-9]+}/delete", s.middlewares.Login(deleteBook)).Methods("POST")
 }
